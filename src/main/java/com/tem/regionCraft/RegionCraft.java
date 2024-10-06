baf0b83024c24680ad00e0a6c03940117321b822
tempoletion <tempoletionchanel@gmail.com>
2024-10-06 12:31:01 +0700

    change mymod java

--- src/main/java/com/tem/regionCraft/RegionCraft.java
+++ src/main/java/com/tem/regionCraft/RegionCraft.java
@@ -10,13 +10,13 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "RegioNCraft", acceptedMinecraftVersions = "[1.7.10]")
 public class MyMod {
 
-    public static final String MODID = "mymodid";
+    public static final String MODID = "regionCraft";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
+    @SidedProxy(clientSide = "com.tem.regionCraft.ClientProxy", serverSide = "com.tem.RegionCraft.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
